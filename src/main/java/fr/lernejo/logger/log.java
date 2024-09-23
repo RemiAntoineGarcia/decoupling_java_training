59e00d11a682b1d2193aabec7b3a8dbaf54b8ae3
remi.garcia <remi.garcia@epita.fr>
2024-09-23 16:16:08 +0200

    part_1

---
+++ src/main/java/fr/lernejo/logger/log.java
@@ -0,0 +1,20 @@
+package fr.lernejo.logger;
+
+
+public interface logger{
+    void log(String message);
+}
+
+public class ConsoleLogger implements logger{
+
+    @java.lang.Override
+    public void log(String message) {
+
+    }
+}
+public class LoggerFactory
+{
+    public static Logger getLogger(String name) {
+
+    }
+}
